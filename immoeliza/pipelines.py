8de3dd5e90bfbc67c4eb1d8b5fcf796a316fff37
denis <dle@verbolia.com>
2023-12-05 08:58:24 +0100

    airflow config

--- immoeliza/pipelines.py
+++ immoeliza/pipelines.py
@@ -25,9 +25,9 @@ class ImmoelizaPipeline:
     def close_spider(self, spider):
         print("SPIDER FINISHED!!!")
         #js=json.load(open("output.json"))
-        df=pd.read_json("output.json")
+        df=pd.read_json("data/output.json")
         df.dropna(subset=["Price","PostalCode"],inplace=True)
         df.drop(df[df["PostalCode"]>10000].index,inplace=True)
         df.drop_duplicate(subset=["Price","Bedrooms","LivingArea"],inplace=True)
-        df.to_json("dataset.json")
+        df.to_json("data/dataset.json",orient="records")
         
